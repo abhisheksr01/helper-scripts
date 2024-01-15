347161b14926ba52e17be825f352d88918b47d91
abhisheksr01 <abhisheksingh2691@gmail.com>
2024-01-15 14:34:05 +0000

    feat: add helper scripts

---
+++ scripts/docker-build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+lint_docker_file() {
+  printMessage "$HEART_ICON" "$HEART_ICON" "Starting hadolinting: $1"
+  hadolint $1
+  printMessage "$HEART_ICON" "$HEART_ICON" "Completed hadolinting: $1"
+}
+
+build_image() {
+  printMessage "$HEART_ICON" "$HEART_ICON" "Start Building Docker Image..."
+  TAG=$([ "${CIRCLE_BRANCH}" == "master" ] && echo "0.1.${CIRCLE_BUILD_NUM}" || echo "${CIRCLE_BRANCH}" | sed 's/dependabot\/gradle//g;s/.//')
+  echo "${TAG}" > version/docker-version.txt
+  echo "${TAG}" > version/test.txt
+  printMessage "$HEART_ICON" "$HEART_ICON" "New Docker Image Version : ${TAG}"
+  echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
+  docker build -t "${DOCKER_USER}/${DOCKER_IMAGE}:${TAG}" -f ci.Dockerfile .
+}
+push_image() {
+  TAG=$(cat version/docker-version.txt)
+  printMessage "$HEART_ICON" "$HEART_ICON" "print value from test.tx"
+  cat version/test.txt
+  if [ "${CIRCLE_BRANCH}" == "master" ]; then
+    printMessage "$HEART_ICON" "$HEART_ICON" "Pushing Docker Image latest & ${TAG} ..."
+    docker tag "${DOCKER_USER}/${DOCKER_IMAGE}:${TAG}" "${DOCKER_USER}/${DOCKER_IMAGE}:latest"
+    docker push "${DOCKER_USER}/${DOCKER_IMAGE}:${TAG}"
+    docker push "${DOCKER_USER}/${DOCKER_IMAGE}:latest"
+    printEmojiWrappedMessage "${GREEN}${HEART_ICON}" "Successfully pushed Docker Image '${DOCKER_USER}/${DOCKER_IMAGE}' with latest & ${TAG} tags..."
+  else
+    printEmojiWrappedMessage "${GREEN}${HEART_ICON}" "Skipping push docker image for non master branch."
+  fi
+}
+"$@"
